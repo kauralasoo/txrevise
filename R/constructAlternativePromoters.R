96cd8fb7a91e9efde09c5b5758a6d30eca0ca0af
Kaur Alasoo <kaur.alasoo@gmail.com>
2018-01-19 17:11:39 +0200

    Add code to construct pure end events.

--- R/constructAlternativePromoters.R
+++ R/constructAlternativePromoters.R
@@ -5,15 +5,21 @@
 #' @return New list of alternative transcript starts where the missing exons have been filled in.
 #' @author Kaur Alasoo
 #' @export
-fillMissingInternalExons <- function(transcripts){
+fillMissingInternalExons <- function(transcripts, type){
   #Identify all unique exons
   all_diff = differenceFromShared(transcripts)
   
-  #Keep unique exons that are not first
-  not_first_unique = listUnion(purrr::map(all_diff, removeFirstExon))
+  #Keep unique exons that are not first or last
+  if(type == "start"){
+    not_first_or_last = listUnion(purrr::map(all_diff, removeFirstExon))
+  } else if(type == "end"){
+    not_first_or_last = listUnion(purrr::map(all_diff, removeLastExon))
+  } else{
+    error("Type has to be either 'start' or end 'end'.")
+  }
   
   #Add missing internal exons
-  new_transcripts = purrr::map(as.list(transcripts), ~addMissingInternalExons(., not_first_unique))
+  new_transcripts = purrr::map(as.list(transcripts), ~addMissingInternalExons(., not_first_or_last))
   
   #Get rid of newly created redundant transcripts
   non_redundant = mergeByMaxDifference(new_transcripts, max_internal_diff = 0, max_start_end_diff = 0)
@@ -30,6 +36,25 @@ removeFirstExon <- function(granges){
     } else{
       #First sort decereasing, then increasing
       new_granges = sort(granges, decreasing = TRUE)
+      new_granges = new_granges[-1]
+      new_granges = sort(new_granges)
+    }
+    return(new_granges)
+  } else {
+    return(granges)
+  }
+}
+
+#Remove first exon from a GRanges object using the correct strand information
+removeLastExon <- function(granges){
+  if(length(granges) > 0){
+    gene_strand = as.character(strand(granges))[1]
+    if(gene_strand == "+"){
+      new_granges = sort(granges, decreasing = TRUE)
+      new_granges = new_granges[-1]
+    } else{
+      #First sort decereasing, then increasing
+      new_granges = sort(granges)
       new_granges = new_granges[-1]
       new_granges = sort(new_granges)
     }
